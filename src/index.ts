62d63de35db1b920abaa4779bb323cce1f87e0f3
JonEast87 <j.d.east@icloud.com>
2023-08-03 18:07:53 -0700

    wrapping up

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
 import { v4 as uuidV4 } from 'uuid';
 
+type Task = {
+  id: string,
+  title: string,
+  completed: boolean,
+  createdAt: Date
+}
+
 const list = document.querySelector<HTMLUListElement>('#list');
 const form = document.querySelector<HTMLFormElement>('#new-task-form');
 const input = document.querySelector<HTMLInputElement>('#new-task-title');
+const tasks: Task[] = [];
 
 form?.addEventListener("submit", e => {
   e.preventDefault();
 
   if (input?.value === "" || input?.value === null) return
 
-  const task = {
+  const newTask: Task = {
     id: uuidV4(),
-    title: input?.value,
+    title: input?.value as string,
     completed: false,
     createdAt: new Date()
   }
+
+  tasks.push(newTask);
+
+  addListItem(newTask);
+  
+  //@ts-ignore
+  input.value = '';
 });
+
+function addListItem(task: Task) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
